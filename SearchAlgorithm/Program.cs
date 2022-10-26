480ceb3f7e03c1546fc41961158183097cb68c2e
Nadiv N <nadiv.n.ft21@mail.umy.ac.id>
2022-10-26 17:02:42 +0700

    fix Char menjadi char

--- SearchAlgorithm/Program.cs
+++ SearchAlgorithm/Program.cs
@@ -109,7 +109,7 @@ namespace SearchAlgorithm
                         + "not found in the array ");
                 Console.WriteLine("\n Number of Comparison: " + ctr);
                 Console.Write("\nContinue search (y/n):");
-                ch = Char.Parse(Console.ReadLine().ToUpper());
+                ch = char.Parse(Console.ReadLine().ToUpper());
             } while ((ch == 'y'));
         }
     }
