a1c4e7ba7135647824eff9606a88d7984f2137d9
Nadiv N <nadiv.n.ft21@mail.umy.ac.id>
2022-10-26 16:40:25 +0700

    add method for input array and add method Binary Search

--- SearchAlgorithm/Program.cs
+++ SearchAlgorithm/Program.cs
@@ -17,10 +17,69 @@ namespace SearchAlgorithm
 
         public void input()
         {
+            while (true)
+            {
+                Console.Write("Enter the number of element in the array: ");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if ((n > 0) && (n <= 20))
+                    break;
+                else
+                    Console.WriteLine
+                        ("\nArray should have minimum 1 and maximum 20 elements");
 
+            }
+            //Accept array elements
+            Console.WriteLine("");
+            Console.WriteLine("----------------");
+            Console.WriteLine("Enter array Elements");
+            Console.WriteLine("----------------");
+            for (i = 0; i < n; i++)
+            { 
+                Console.Write("<"+(i+1)+">");
+                string s1 = Console.ReadLine();
+                arr[i] = Int32.Parse(s1);
+            }
         }
-        static void Main(string[] args)
+        public void BinarySearch()
         {
+            char ch;
+            do
+            {
+                // accept the number to be searched
+                Console.Write
+                    ("\nEnter element want you to search :");
+                int item = Convert.ToInt32(Console.ReadLine());
+
+                // Apply binary search
+                int lowerbound = 0;
+                int upperbound = n - 1;
+
+                //obtain the index of the middle elements
+                int mid = (upperbound + lowerbound) / 2;
+                int ctr = 1;
+
+                //loop to search for the elements in the array
+                while ((item != arr[mid]) && (lowerbound <= upperbound))
+                {
+                    if (item > arr[mid])
+                        lowerbound = mid + 1;
+                    else
+                        upperbound = mid - 1;
+                    mid = (upperbound + lowerbound) / 2;
+                    ctr++;
+                }
+                if (item == arr[mid])
+                    Console.WriteLine("\n" + item.ToString() + "found at position"
+                        + (mid + 1).ToString());
+                else
+                    Console.WriteLine("\n" + item.ToString()
+                        + "not found in the array\n");
+                Console.WriteLine("\nNumber of Comparison :" + ctr);
+
+                Console.Write("\nContinue search (y/n:");
+                ch = char.Parse(Console.ReadLine().ToUpper());
+            } while ((ch == 'y'));
         }
     }
 }
